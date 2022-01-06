00b093718065d652c1f952d000cc862ad80366c1
umang.gupta <umang.gupta@milliman.com>
2022-01-06 17:56:48 -0500

    black test script

--- python/tests/test_decorator.py
+++ python/tests/test_decorator.py
@@ -11,10 +11,8 @@
 # pylint: disable=redefined-outer-name
 
 from pathlib import Path
-import datetime
 
 import pytest
-from pyspark import Row
 import pyspark.sql.functions as spark_funcs
 
 import pref_sens_surg.decorator
@@ -43,7 +41,7 @@ def mock_schemas():
     return {
         path_.stem: build_structtype_from_csv(path_)
         for path_ in PATH_MOCK_SCHEMAS.glob("*.csv")
-        }
+    }
 
 
 @pytest.fixture
@@ -56,35 +54,40 @@ def mock_dataframes(spark_app, mock_schemas):
             sep=",",
             header=True,
             mode="FAILFAST",
-            )
+        )
         for path_ in PATH_MOCK_DATA.glob("*.csv")
-        }
+    }
+
 
 def test_pss(mock_dataframes):
     """Test the avoidable claim identification"""
     test_instance = pref_sens_surg.decorator.PSSDecorator()
-    actual_result = test_instance.calc_decorator(dfs_input=mock_dataframes, dfs_refs=mock_dataframes)
+    actual_result = test_instance.calc_decorator(
+        dfs_input=mock_dataframes, dfs_refs=mock_dataframes
+    )
     actual_result.cache()
-    expected_result = mock_dataframes["ccs_results"].select(
-        'sequencenumber',
-        *[
-            spark_funcs.col(col).alias('expected_' + col)
-            for col in mock_dataframes["ccs_results"].columns
-            if col.startswith('ccs')
+    expected_result = (
+        mock_dataframes["ccs_results"]
+        .select(
+            "sequencenumber",
+            *[
+                spark_funcs.col(col).alias("expected_" + col)
+                for col in mock_dataframes["ccs_results"].columns
+                if col.startswith("ccs")
             ]
-    ).cache()
+        )
+        .cache()
+    )
     assert actual_result.count() == expected_result.count()
 
     compare = expected_result.join(
-        actual_result,
-        test_instance.key_fields,
-        "left_outer",
-        ).cache()
+        actual_result, test_instance.key_fields, "left_outer"
+    ).cache()
     compare_fields = {
-        exp_col: exp_col[len("expected_"):]
+        exp_col: exp_col[len("expected_") :]
         for exp_col in expected_result.columns
         if exp_col.startswith("expected_")
-        }
+    }
     failures = list()
     failure_rows = set()
     for expected_column, actual_column in compare_fields.items():
@@ -92,8 +95,11 @@ def test_pss(mock_dataframes):
         n_misses = misses.count()
         if n_misses != 0:
             failures.append(actual_column)
-            failure_rows = failure_rows | set([row['sequencenumber'] for row in misses.collect()])
-    assert not failures, "Unexpected values in '{}' column(s) for sequencenumbers '{}'".format(
-        failures,
-        failure_rows,
-        )
+            failure_rows = failure_rows | set(
+                [row["sequencenumber"] for row in misses.collect()]
+            )
+    assert (
+        not failures
+    ), "Unexpected values in '{}' column(s) for sequencenumbers '{}'".format(
+        failures, failure_rows
+    )
